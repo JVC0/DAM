bd09280b9b056cfee83f141e24353b39765aabf8
JVC0 <josephvanegas48@gmail.com>
2025-12-15 02:30:23 +0000

    FIXED

--- AED - Acceso de Datos/Tarea2/src/servicio/servicioTareas.ts
+++ AED - Acceso de Datos/Tarea2/src/servicio/servicioTareas.ts
@@ -1,12 +1,31 @@
 import { FiltroTarea, IdTarea, Tarea } from "../model/tarea";
+import { RepositorioTareaRest } from "../repositorio/apiTareasRemota";
 import { RespositorioTareaSqlite } from "../repositorio/repositorioTareasSqlite";
 
+export type OrigenDatos = "local" | "remoto";
 
 export class ServicioTareas {
-  constructor(private repo: RespositorioTareaSqlite) {}
+  private repoLocal: RespositorioTareaSqlite;
+  private repoRemoto: RepositorioTareaRest;
+  private origenActual: OrigenDatos;
 
-  listar(filtro: FiltroTarea): Tarea[] {
-    const todas = this.repo.obtenerTodas();
+  constructor(
+    repoLocal: RespositorioTareaSqlite,
+    repoRemoto: RepositorioTareaRest,
+    origenInicial: OrigenDatos = "local"
+  ) {
+    this.repoLocal = repoLocal;
+    this.repoRemoto = repoRemoto;
+    this.origenActual = origenInicial;
+  }
+
+
+  async listar(filtro: FiltroTarea): Promise<Tarea[]> {
+    const todas =
+      this.origenActual === "local"
+        ? this.repoLocal.obtenerTodas()
+        : await this.repoRemoto.obtenerTodas();
+
     switch (filtro) {
       case "pendientes":
         return todas.filter((t) => !t.completada);
@@ -18,29 +37,62 @@ export class ServicioTareas {
     }
   }
 
-  crear(titulo: string, descripcion?: string): Tarea {
+  async crear(titulo: string, descripcion?: string): Promise<Tarea> {
     if (!titulo || titulo.trim().length === 0) {
       throw new Error("El título no puede estar vacío");
     }
-    return this.repo.crear(titulo, descripcion);
-  }
-  obtenerPorId(id: IdTarea): Tarea | undefined {
-    if (id < 0) {
-      throw new Error("EL id no puede ser menor que 0");
+
+    if (this.origenActual === "local") {
+      return this.repoLocal.crear(titulo, descripcion);
+    } else {
+      return await this.repoRemoto.crear({
+        titulo,
+        descripcion,
+        completada: false,
+      });
     }
-    return this.repo.obtenerPorId(id);
   }
-  actualizar(tarea: Tarea): Tarea | undefined {
-    if (tarea == null) {
-      throw new Error("EL id no puede ser menor que 0");
+
+  async obtenerPorId(id: IdTarea): Promise<Tarea | undefined> {
+    if (id < 0) {
+      throw new Error("El id no puede ser menor que 0");
     }
 
-    return this.repo.actualizar(tarea);
+    return this.origenActual === "local"
+      ? this.repoLocal.obtenerPorId(id)
+      : await this.repoRemoto.obtenerPorId(id);
   }
-  borrar(id: IdTarea): boolean {
-    if (id < 0) {
-      throw new Error("EL id no puede ser menor que 0");
+
+  async actualizar(tarea: Tarea): Promise<Tarea | undefined> {
+    if (tarea == null) {
+      throw new Error("La tarea no puede ser null");
     }
-    return this.repo.borrar(id);
+
+    return this.origenActual === "local"
+      ? this.repoLocal.actualizar(tarea)
+      : await this.repoRemoto.actualizar(tarea);
   }
+
+  async borrar(id: IdTarea): Promise<boolean> {
+    if (id < 0) {
+      throw new Error("El id no puede ser menor que 0");
+    }
+
+    return this.origenActual === "local"
+? this.repoLocal.borrar(id)
+      : await this.repoRemoto.borrar(id);
+  }
+   async sincronizarRemotoALocal(): Promise<void> {
+    const tareasRemotas = await this.repoRemoto.obtenerTodas();
+    const tareasLocales = this.repoLocal.obtenerTodas();
+
+    for (const tareaLocal of tareasLocales) {
+      this.repoLocal.borrar(tareaLocal.id);
+    }
+
+    for (const tareaRemota of tareasRemotas) {
+      this.repoLocal.crear(tareaRemota.titulo, tareaRemota.descripcion);
+    }
+  }
+
 }
